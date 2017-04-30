fb4d6345df97b4385a3460663096b0dfd1dc05c9
Sam Eppinger <samantha.eppinger@students.olin.edu>
2017-04-30 10:10:30 -0400

    This is my MP5 completed!

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,25 @@
 """
-Put your Flask app code here.
+This is my flask code that makes my website, the "result page", and the "not enough information page"
 """
+from flask import Flask
+from flask import render_template
+from flask import request
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST', 'GET'])
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/result', methods = ['POST'])
+def result():
+   error = None
+   if request.method == 'POST':
+      if request.form["Name"] and request.form["Age"] and request.form["Who is your favorite SoftDes Ninja"]:
+        error = None
+        return render_template("result.html", Name = request.form["Name"], Age = request.form["Age"])
+      else:
+       error = 'Please Fill In Fields'
+       return render_template('neip.html')
+
+if __name__ == '__main__':
+    app.run()
--- hello.py
+++ hello.py
@@ -3,11 +3,22 @@ Simple "Hello, World" application using Flask
 """
 
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+    # return 'This is my Index Page: hi from sam'
+
+# @app.route('/hello')
+# def hello():
+#     return 'Hello World'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run()
